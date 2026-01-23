0362d0161ca4223132ae54fbd1d7e2d0f43fa923
pare1610 <pare1610@outlook.com>
2026-01-23 14:39:07 -0500

    Primer

---
+++ src/main/java/com/proelectricos/restintce/RestIntCeApplication.java
@@ -0,0 +1,13 @@
+package com.proelectricos.restintce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestIntCeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestIntCeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proelectricos/restintce/RestIntCeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proelectricos.restintce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestIntCeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
